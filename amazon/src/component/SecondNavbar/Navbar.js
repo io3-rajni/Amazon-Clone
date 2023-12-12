977c88e9f2f31664c95fdd4982315d42ee015788
Rajni <io3.rajni@gmail.com>
2023-12-12 21:45:56 +0530

    second navbar process

--- amazon/src/component/SecondNavbar/Navbar.js
+++ amazon/src/component/SecondNavbar/Navbar.js
@@ -1,55 +1,118 @@
 import * as React from "react";
-
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
 import { Typography } from "@mui/material";
 import Box from "@mui/material/Box";
 import Link from "@mui/material/Link";
-
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import "../Navbar/mainnavbar.css";
+import { NavlinkData } from "./NavlinkData";
 const preventDefault = (event) => event.preventDefault();
 
 const Navbar = () => {
   return (
     <>
-      <Typography component="div" sx={{ display: "flex" }}>
-        <IconButton
-          // size="large"
-          edge="start"
-          color="inherit"
-          aria-label="menu"
-          sx={{ mr: 2 }}
-        >
-          <MenuIcon />
-        </IconButton>
-        <Typography sx={{ paddingTop: "10px", listStyleType: "none" }}>
-          <Box
-            sx={{
-              typography: "body1",
-              "& > :not(style) ~ :not(style)": {
-                ml: 2,
-              },
-            }}
-            onClick={preventDefault}
+      <Box
+        sx={{
+          // typography: "body1",
+          // "& > :not(style) ~ :not(style)": {
+          //   ml: 2,
+          //   fontSize: "14px",
+          //   marginLeft: "15px",
+          // },
+          display: "flex",
+          backgroundColor: "#131921",
+        }}
+        onClick={preventDefault}
+      >
+        <Link href="#" underline="none" color="#fff">
+          <IconButton
+            // size="large"
+            // edge="start"
+            // color="inherit"
+            // aria-label="menu"
+            sx={{ ml: 3, color: "#fff", p: 0 }}
           >
-            <Link href="#">Link</Link>
-            <Link href="#">
-              Link
-              {/* {'color="inherit"'} */}
-            </Link>
-            <Link href="#">
-              Link
-              {/* {'variant="body2"'} */}
-            </Link>
-            <Link href="#">Link</Link>
-            <Link href="#">Link</Link>
-            <Link href="#">Link</Link>
-            <Link href="#">Link</Link>
-            <Link href="#">Link</Link>
-            <Link href="#">Link</Link>
-            <Link href="#">Link</Link>
-          </Box>
-        </Typography>
-      </Typography>
+            <MenuIcon />
+            <Typography component="span" sx={{ m: 2, lineHeight: 0.5 }}>
+              All
+            </Typography>
+          </IconButton>
+          {NavlinkData?.map((item, i) => {
+            return (
+              <>
+                {item?.btn ? (
+                  <Tooltip title="Add" arrow>
+                    <Button
+                      className="tooltips"
+                      sx={{ textTransform: "capitalize", color: "#fff" }}
+                    >
+                      {item?.title} <ArrowDropDownIcon />
+                    </Button>
+                  </Tooltip>
+                ) : (
+                  <Link href="#" underline="none" color="#fff" sx={{ m: 0.8 }}>
+                    {item?.title}
+                    {/* {'color="inherit"'} */}
+                  </Link>
+                )}
+              </>
+            );
+          })}
+        </Link>
+        {/* <Link href="#" underline="none" color="#fff" sx={{ m: 0.8 }}>
+          Amazon miniTV
+        </Link>
+        <Link href="#" underline="none" color="#fff" sx={{ m: 0.8 }}>
+          Sell
+        </Link>
+        <Link href="#" underline="none" color="#fff" sx={{ m: 0.8 }}>
+          Best Sellers
+        </Link>
+        <Link href="#" underline="none" color="#fff" sx={{ m: 0.8 }}>
+          Today's Deals
+        </Link>
+        <Link href="#" underline="none" color="#fff" sx={{ m: 0.8 }}>
+          Mobiles
+        </Link>
+        <Link href="#" underline="none" color="#fff" sx={{ m: 0.8 }}>
+          Customer Services
+        </Link>
+        <Link href="#" underline="none" color="#fff" sx={{ m: 0.8 }}>
+          Electronic
+        </Link>
+        <Tooltip title="Add" arrow>
+          <Button
+            className="tooltips"
+            sx={{ textTransform: "capitalize", color: "#fff" }}
+          >
+            Arrow <ArrowDropDownIcon />
+          </Button>
+        </Tooltip>
+        <Link href="#" underline="none" color="#fff">
+          New Released
+        </Link>
+        <Link href="#" underline="none" color="#fff">
+          Home&Kitchen
+        </Link>
+        <Link href="#" underline="none" color="#fff">
+          Gift Ideas
+        </Link>
+        <Link href="#" underline="none" color="#fff">
+          Gift Ideas
+        </Link>
+        <Link
+          href="#"
+          underline="none"
+          // color="#fff"
+          ml=" 6"
+          //              sx={{ , marginLeft: "67px", fontSize: "21px", color: "red" }}
+        >
+          Shopping made easy | Download the App
+        </Link> */}
+      </Box>
     </>
   );
 };
---
+++ amazon/src/component/SecondNavbar/NavlinkData.js
@@ -0,0 +1,63 @@
+export const NavlinkData = [
+  {
+    id: "1",
+    title: "Amazon miniTV",
+    btn: false,
+  },
+  {
+    id: "2",
+    btn: false,
+    title: "Sell",
+  },
+  {
+    id: "3",
+    btn: false,
+    title: "Best Sellers",
+  },
+  {
+    id: "4",
+    btn: false,
+
+    title: "Today's Deals",
+  },
+  {
+    id: "5",
+    btn: false,
+
+    title: "Mobiles",
+  },
+  {
+    id: "6",
+    btn: false,
+
+    title: "Customer Services",
+  },
+  {
+    id: "7",
+    btn: false,
+
+    title: "Electronic",
+  },
+  {
+    id: "8",
+    btn: false,
+
+    title: "New Released",
+  },
+  {
+    id: "9",
+    btn: true,
+    title: "Prime",
+  },
+  {
+    id: "9",
+    btn: false,
+    title: "Home&Kitchen",
+  },
+  {
+    id: "10",
+    btn: false,
+
+    title: "Gift Ideas",
+  },
+];
