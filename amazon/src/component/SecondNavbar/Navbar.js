0fab8e0cea45c3757b1580d9a024415a5c9573ad
Rajni <io3.rajni@gmail.com>
2023-12-13 00:17:49 +0530

    complete navbar

--- amazon/src/component/SecondNavbar/Navbar.js
+++ amazon/src/component/SecondNavbar/Navbar.js
@@ -9,6 +9,7 @@ import Link from "@mui/material/Link";
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 import "../Navbar/mainnavbar.css";
 import { NavlinkData } from "./NavlinkData";
+import shoppingEsay from "../../assets/shopping esay.jpg";
 const preventDefault = (event) => event.preventDefault();
 
 const Navbar = () => {
@@ -16,27 +17,20 @@ const Navbar = () => {
     <>
       <Box
         sx={{
-          // typography: "body1",
-          // "& > :not(style) ~ :not(style)": {
-          //   ml: 2,
-          //   fontSize: "14px",
-          //   marginLeft: "15px",
-          // },
           display: "flex",
-          backgroundColor: "#131921",
         }}
         onClick={preventDefault}
       >
-        <Link href="#" underline="none" color="#fff">
-          <IconButton
-            // size="large"
-            // edge="start"
-            // color="inherit"
-            // aria-label="menu"
-            sx={{ ml: 3, color: "#fff", p: 0 }}
-          >
+        <Link
+          href="#"
+          underline="none"
+          color="#fff"
+          backgroundColor="#203040"
+          fontSize="14px"
+        >
+          <IconButton sx={{ ml: 3, color: "#fff", p: 0 }}>
             <MenuIcon />
-            <Typography component="span" sx={{ m: 2, lineHeight: 0.5 }}>
+            <Typography component="span" sx={{ m: 1, lineHeight: 1 }}>
               All
             </Typography>
           </IconButton>
@@ -53,65 +47,30 @@ const Navbar = () => {
                     </Button>
                   </Tooltip>
                 ) : (
-                  <Link href="#" underline="none" color="#fff" sx={{ m: 0.8 }}>
+                  <Link
+                    href="#"
+                    underline="none"
+                    color="#fff"
+                    sx={{ p: 1.3, width: " 78%" }}
+                    className="services"
+                  >
                     {item?.title}
-                    {/* {'color="inherit"'} */}
                   </Link>
                 )}
               </>
             );
           })}
         </Link>
-        {/* <Link href="#" underline="none" color="#fff" sx={{ m: 0.8 }}>
-          Amazon miniTV
-        </Link>
-        <Link href="#" underline="none" color="#fff" sx={{ m: 0.8 }}>
-          Sell
-        </Link>
-        <Link href="#" underline="none" color="#fff" sx={{ m: 0.8 }}>
-          Best Sellers
-        </Link>
-        <Link href="#" underline="none" color="#fff" sx={{ m: 0.8 }}>
-          Today's Deals
-        </Link>
-        <Link href="#" underline="none" color="#fff" sx={{ m: 0.8 }}>
-          Mobiles
-        </Link>
-        <Link href="#" underline="none" color="#fff" sx={{ m: 0.8 }}>
-          Customer Services
-        </Link>
-        <Link href="#" underline="none" color="#fff" sx={{ m: 0.8 }}>
-          Electronic
-        </Link>
-        <Tooltip title="Add" arrow>
-          <Button
-            className="tooltips"
-            sx={{ textTransform: "capitalize", color: "#fff" }}
-          >
-            Arrow <ArrowDropDownIcon />
-          </Button>
-        </Tooltip>
-        <Link href="#" underline="none" color="#fff">
-          New Released
-        </Link>
-        <Link href="#" underline="none" color="#fff">
-          Home&Kitchen
-        </Link>
-        <Link href="#" underline="none" color="#fff">
-          Gift Ideas
-        </Link>
-        <Link href="#" underline="none" color="#fff">
-          Gift Ideas
-        </Link>
-        <Link
-          href="#"
-          underline="none"
-          // color="#fff"
-          ml=" 6"
-          //              sx={{ , marginLeft: "67px", fontSize: "21px", color: "red" }}
-        >
-          Shopping made easy | Download the App
-        </Link> */}
+        <Typography
+          component="div"
+          sx={{ width: "2%", backgroundColor: "#203040" }}
+        ></Typography>
+        <img
+          src={shoppingEsay}
+          alt="image"
+          sx={{ width: "21%" }}
+          className="shoppingEasyLine"
+        />
       </Box>
     </>
   );
