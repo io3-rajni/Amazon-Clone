df1d7afb8970729bbc58509522a5d001be76ad8f
Rajni <io3.rajni@gmail.com>
2024-01-15 00:06:12 +0530

    back to top button and window scrollto

--- amazon/src/component/Footer/Footer.jsx
+++ amazon/src/component/Footer/Footer.jsx
@@ -8,153 +8,322 @@ import {
   letUsHelpYou,
 } from "../../component/DataConfig";
 const Footer = () => {
+  const scrollTo = () => {
+    window.scrollTo({
+      top: 0,
+      left: 100,
+      behavior: "smooth",
+    });
+  };
   return (
-    <Grid
-      container
-      spacing={{ xs: 2, md: 3 }}
-      columns={{ xs: 4, sm: 8, md: 12 }}
-      sx={{
-        display: "flex",
-        justifyContent: "center",
-        margin: "82%",
-        margin: "auto",
-        fontSize: "14px",
-        background: "#232F3E",
-        marginTop: "1px !important",
-      }}
-    >
-      <Grid item sx={6} sm={4} md={2}>
-        {getToKnowUs.map((item, index) => {
-          return (
-            <>
-              <Typography
-                key={index}
-                sx={{
-                  fontSize: "14px",
-                  fontWeight: "900",
-                  color: "#fff",
-                }}
-              >
-                {item?.title}
-              </Typography>
-              {item.child.map((item, index) => {
-                return (
-                  <Typography
-                    sx={{
-                      fontSize: "14px",
-                      lineHeight: "2rem !important",
-                      color: "#fff",
-                    }}
-                    key={index}
-                  >
-                    {item.option}
-                  </Typography>
-                );
-              })}
-            </>
-          );
-        })}
+    <>
+      <Typography
+        varient="h5"
+        className="backToTop"
+        onClick={scrollTo}
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          height: "59px",
+          alignItems: "center",
+          color: "#fff",
+          background: "#37475A",
+        }}
+      >
+        Back of top
+      </Typography>
+
+      <Grid
+        container
+        spacing={{ xs: 2, md: 3 }}
+        columns={{ xs: 4, sm: 8, md: 12 }}
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          margin: "82%",
+          margin: "auto",
+          fontSize: "14px",
+          background: "#232F3E",
+          marginTop: "1px !important",
+        }}
+      >
+        <Grid item sx={6} sm={4} md={2}>
+          {getToKnowUs.map((item, index) => {
+            return (
+              <>
+                <Typography
+                  key={index}
+                  sx={{
+                    fontSize: "14px",
+                    fontWeight: "900",
+                    color: "#fff",
+                  }}
+                >
+                  {item?.title}
+                </Typography>
+                {item.child.map((item, index) => {
+                  return (
+                    <Typography
+                      sx={{
+                        fontSize: "14px",
+                        lineHeight: "2rem !important",
+                        color: "#fff",
+                      }}
+                      key={index}
+                    >
+                      {item.option}
+                    </Typography>
+                  );
+                })}
+              </>
+            );
+          })}
+        </Grid>
+        <Grid item sx={6} sm={4} md={2}>
+          {connectWith.map((item, index) => {
+            return (
+              <>
+                <Typography
+                  key={index}
+                  sx={{
+                    fontSize: "14px",
+                    fontWeight: "900",
+                    color: "#fff",
+                  }}
+                >
+                  {item?.title}
+                </Typography>
+                {item.child.map((item, index) => {
+                  return (
+                    <Typography
+                      key={index}
+                      sx={{
+                        fontSize: "14px",
+                        lineHeight: "2rem !important",
+                        color: "#fff",
+                      }}
+                    >
+                      {item.option}
+                    </Typography>
+                  );
+                })}
+              </>
+            );
+          })}
+        </Grid>
+        <Grid item xs={2} sm={4} md={2}>
+          {makeMoneyWithUs.map((item, index) => {
+            return (
+              <>
+                <Typography
+                  key={index}
+                  sx={{
+                    fontSize: "14px",
+                    fontWeight: "900",
+                    color: "#fff",
+                  }}
+                >
+                  {" "}
+                  {item.title}
+                </Typography>
+                {item.child.map((item, index) => {
+                  return (
+                    <Typography
+                      key={index}
+                      sx={{
+                        fontSize: "14px",
+                        lineHeight: "2rem !important",
+                        color: "#fff",
+                      }}
+                    >
+                      {item?.option}
+                    </Typography>
+                  );
+                })}
+              </>
+            );
+          })}
+        </Grid>
+        <Grid item xs={2} sm={4} md={2}>
+          {letUsHelpYou.map((item, index) => {
+            return (
+              <>
+                <Typography
+                  key={index}
+                  sx={{
+                    fontSize: "14px",
+                    fontWeight: "900",
+                    color: "#fff",
+                  }}
+                >
+                  {" "}
+                  {item.title}
+                </Typography>
+                {item.child.map((item, index) => {
+                  return (
+                    <Typography
+                      key={index}
+                      sx={{
+                        fontSize: "14px",
+                        lineHeight: "2rem !important",
+                        color: "#fff",
+                      }}
+                    >
+                      {item?.option}
+                    </Typography>
+                  );
+                })}
+              </>
+            );
+          })}
+        </Grid>
       </Grid>
-      <Grid item sx={6} sm={4} md={2}>
-        {connectWith.map((item, index) => {
-          return (
-            <>
-              <Typography
-                key={index}
-                sx={{
-                  fontSize: "14px",
-                  fontWeight: "900",
-                  color: "#fff",
-                }}
-              >
-                {item?.title}
-              </Typography>
-              {item.child.map((item, index) => {
-                return (
-                  <Typography
-                    key={index}
-                    sx={{
-                      fontSize: "14px",
-                      lineHeight: "2rem !important",
-                      color: "#fff",
-                    }}
-                  >
-                    {item.option}
-                  </Typography>
-                );
-              })}
-            </>
-          );
-        })}
+      <Grid
+        container
+        spacing={{ xs: 2, md: 3 }}
+        columns={{ xs: 4, sm: 8, md: 12 }}
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          margin: "82%",
+          margin: "auto",
+          fontSize: "14px",
+          background: "#232F3E",
+          marginTop: "1px !important",
+        }}
+      >
+        <Grid item sx={6} sm={4} md={2}>
+          {getToKnowUs.map((item, index) => {
+            return (
+              <>
+                <Typography
+                  key={index}
+                  sx={{
+                    fontSize: "14px",
+                    fontWeight: "900",
+                    color: "#fff",
+                  }}
+                >
+                  {item?.title}
+                </Typography>
+                {item.child.map((item, index) => {
+                  return (
+                    <Typography
+                      sx={{
+                        fontSize: "14px",
+                        lineHeight: "2rem !important",
+                        color: "#fff",
+                      }}
+                      key={index}
+                    >
+                      {item.option}
+                    </Typography>
+                  );
+                })}
+              </>
+            );
+          })}
+        </Grid>
+        <Grid item sx={6} sm={4} md={2}>
+          {connectWith.map((item, index) => {
+            return (
+              <>
+                <Typography
+                  key={index}
+                  sx={{
+                    fontSize: "14px",
+                    fontWeight: "900",
+                    color: "#fff",
+                  }}
+                >
+                  {item?.title}
+                </Typography>
+                {item.child.map((item, index) => {
+                  return (
+                    <Typography
+                      key={index}
+                      sx={{
+                        fontSize: "14px",
+                        lineHeight: "2rem !important",
+                        color: "#fff",
+                      }}
+                    >
+                      {item.option}
+                    </Typography>
+                  );
+                })}
+              </>
+            );
+          })}
+        </Grid>
+        <Grid item xs={2} sm={4} md={2}>
+          {makeMoneyWithUs.map((item, index) => {
+            return (
+              <>
+                <Typography
+                  key={index}
+                  sx={{
+                    fontSize: "14px",
+                    fontWeight: "900",
+                    color: "#fff",
+                  }}
+                >
+                  {" "}
+                  {item.title}
+                </Typography>
+                {item.child.map((item, index) => {
+                  return (
+                    <Typography
+                      key={index}
+                      sx={{
+                        fontSize: "14px",
+                        lineHeight: "2rem !important",
+                        color: "#fff",
+                      }}
+                    >
+                      {item?.option}
+                    </Typography>
+                  );
+                })}
+              </>
+            );
+          })}
+        </Grid>
+        <Grid item xs={2} sm={4} md={2}>
+          {letUsHelpYou.map((item, index) => {
+            return (
+              <>
+                <Typography
+                  key={index}
+                  sx={{
+                    fontSize: "14px",
+                    fontWeight: "900",
+                    color: "#fff",
+                  }}
+                >
+                  {" "}
+                  {item.title}
+                </Typography>
+                {item.child.map((item, index) => {
+                  return (
+                    <Typography
+                      key={index}
+                      sx={{
+                        fontSize: "14px",
+                        lineHeight: "2rem !important",
+                        color: "#fff",
+                      }}
+                    >
+                      {item?.option}
+                    </Typography>
+                  );
+                })}
+              </>
+            );
+          })}
+        </Grid>
       </Grid>
-      <Grid item xs={2} sm={4} md={2}>
-        {makeMoneyWithUs.map((item, index) => {
-          return (
-            <>
-              <Typography
-                key={index}
-                sx={{
-                  fontSize: "14px",
-                  fontWeight: "900",
-                  color: "#fff",
-                }}
-              >
-                {" "}
-                {item.title}
-              </Typography>
-              {item.child.map((item, index) => {
-                return (
-                  <Typography
-                    key={index}
-                    sx={{
-                      fontSize: "14px",
-                      lineHeight: "2rem !important",
-                      color: "#fff",
-                    }}
-                  >
-                    {item?.option}
-                  </Typography>
-                );
-              })}
-            </>
-          );
-        })}
-      </Grid>
-      <Grid item xs={2} sm={4} md={2}>
-        {letUsHelpYou.map((item, index) => {
-          return (
-            <>
-              <Typography
-                key={index}
-                sx={{
-                  fontSize: "14px",
-                  fontWeight: "900",
-                  color: "#fff",
-                }}
-              >
-                {" "}
-                {item.title}
-              </Typography>
-              {item.child.map((item, index) => {
-                return (
-                  <Typography
-                    key={index}
-                    sx={{
-                      fontSize: "14px",
-                      lineHeight: "2rem !important",
-                      color: "#fff",
-                    }}
-                  >
-                    {item?.option}
-                  </Typography>
-                );
-              })}
-            </>
-          );
-        })}
-      </Grid>
-    </Grid>
-    // </Box>
+    </>
   );
 };
 export default Footer;
