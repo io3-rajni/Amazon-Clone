f943f1285a26e30f82a7b75161c86441633900f0
Rajni <io3.rajni@gmail.com>
2024-01-24 23:37:46 +0530

    Country Dropdown Button

--- amazon/src/component/Header/Navbar/CountryButton.jsx
+++ amazon/src/component/Header/Navbar/CountryButton.jsx
@@ -1,29 +1,77 @@
 import React, { useState } from "react";
 import { useEffect } from "react";
 import axios from "axios";
-import { Button } from "@mui/material";
+import Button from "@mui/material/Button";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import Fade from "@mui/material/Fade";
+import { Typography } from "@mui/material";
 
 const CountryButton = () => {
   const [apiData, setApiData] = useState([]);
-  console.log("api", apiData);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  // const [countryData, setCountryData] = useState(anchorEl);
   useEffect(() => {
     axios
       .get("https://restcountries.com/v3.1/all")
       .then((response) => {
-        setApiData(response?.data?.flag?.svg);
+        setApiData(response?.data);
         console.log("res", response);
       })
       .catch((error) => {
         console.log("error", error);
       });
-  });
-  const handleApi = () => {
-    setApiData(true);
+  }, []);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
   };
-  console.log(apiData);
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  // const handleData = (e) => {
+  //   setCountryData(e?.target?.value);
+  // };
+
   return (
     <>
-      <Button onClick={handleApi}>xux</Button>
+      <Button
+        id="fade-button"
+        aria-controls={open ? "fade-menu" : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? "true" : undefined}
+        onClick={handleClick}
+        sx={{ color: "white", textTransform: "capitalize" }}
+        className="countryDropdown"
+      >
+        Country
+        {/* {countryData} */}
+      </Button>
+      <Menu
+        id="fade-menu"
+        MenuListProps={{
+          "aria-labelledby": "fade-button",
+        }}
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        TransitionComponent={Fade}
+      >
+        {apiData.map((item, index) => {
+          console.log("item in menu", item);
+          return (
+            <MenuItem onClick={handleClose} sx={{ width: "9.8rem" }}>
+              {/* <Typography onChange={handleData}> */}
+              <img
+                src={item?.flags?.svg}
+                style={{ height: "12px", width: "14px" }}
+              />
+              {item?.name?.common}
+              {/* </Typography> */}
+            </MenuItem>
+          );
+        })}
+      </Menu>
     </>
   );
 };
