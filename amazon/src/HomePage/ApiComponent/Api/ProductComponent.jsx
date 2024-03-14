d31a96359f6da5179e5cbde655cc275c7c81ef19
Rajni <io3.rajni@gmail.com>
2024-03-14 17:59:29 +0530

    added loader

--- amazon/src/HomePage/ApiComponent/Api/ProductComponent.jsx
+++ amazon/src/HomePage/ApiComponent/Api/ProductComponent.jsx
@@ -38,105 +38,107 @@ const ProductComponent = () => {
   console.log("hhgf", productData.products);
   return (
     <>
-      <Box sx={{ display: "flex" }}>
-        <CircularProgress />
-      </Box>
-
-      <Box sx={{ flexGrow: 1 }}>
-        <Grid
-          container
-          spacing={{ xs: 2, md: 4 }}
-          columns={{ xs: 4, sm: 8, md: 12 }}
-        >
-          {/* {Array.from(Array(6)).map((_, index) => ( */}
-          {productData?.products?.map((item, index) => {
-            console.log("item", item);
-            return (
-              <>
-                <Grid item xs={2} sm={3} md={3} key={index}>
-                  <Card sx={{ maxWidth: 300 }}>
-                    <CardActionArea>
-                      <CardMedia
-                        component="img"
-                        height="140"
-                        image={item?.thumbnail}
-                        alt=""
-                      />
-                      <CardContent>
-                        {/* <Typography gutterBottom variant="h5" component="div">
-                          Lizard
-                        </Typography> */}
-                        <Typography variant="body2" color="text.secondary">
-                          {item?.description}
-                        </Typography>
-                      </CardContent>
-                    </CardActionArea>
-                    <CardActions sx={{ display: "block" }}>
-                      <Typography
-                        variant="h6"
-                        color="text.secondary"
-                        sx={{ display: "flex" }}
-                      >
-                        <Typography>₹</Typography>
-                        <Typography variant="h4" sx={{ color: "#353434" }}>
-                          {item?.price}
-                        </Typography>
-                        <Typography sx={{ marginLeft: "10px" }}>
-                          {item?.category}
-                        </Typography>
-                      </Typography>
-                      <Typography variant="h5" sx={{ display: "flex" }}>
-                        <Rating
-                          name="simple-controlled"
-                          value={value}
-                          onChange={(event, newValue) => {
-                            setValue(newValue);
-                          }}
+      {productData ? (
+        <Box sx={{ flexGrow: 1 }}>
+          <Grid
+            container
+            spacing={{ xs: 2, md: 4 }}
+            columns={{ xs: 4, sm: 8, md: 12 }}
+          >
+            {/* {Array.from(Array(6)).map((_, index) => ( */}
+            {productData?.products?.map((item, index) => {
+              console.log("item", item);
+              return (
+                <>
+                  <Grid item xs={2} sm={3} md={3} key={index}>
+                    <Card sx={{ maxWidth: 300 }}>
+                      <CardActionArea>
+                        <CardMedia
+                          component="img"
+                          height="140"
+                          image={item?.thumbnail}
+                          alt=""
                         />
+                        <CardContent>
+                          {/* <Typography gutterBottom variant="h5" component="div">
+                        Lizard
+                      </Typography> */}
+                          <Typography variant="body2" color="text.secondary">
+                            {item?.description}
+                          </Typography>
+                        </CardContent>
+                      </CardActionArea>
+                      <CardActions sx={{ display: "block" }}>
                         <Typography
-                          component="div"
-                          sx={{
-                            marginLeft: "20px",
-                            color: "#007185",
-                            fontSize: "12px",
-                          }}
+                          variant="h6"
+                          color="text.secondary"
+                          sx={{ display: "flex" }}
                         >
-                          {item?.rating}
+                          <Typography>₹</Typography>
+                          <Typography variant="h4" sx={{ color: "#353434" }}>
+                            {item?.price}
+                          </Typography>
+                          <Typography sx={{ marginLeft: "10px" }}>
+                            {item?.category}
+                          </Typography>
                         </Typography>
-                      </Typography>
-                      <Typography variant="h5" sx={{ display: "flex" }}>
-                        <Button
-                          sx={{
-                            size: "small",
-
-                            background: "#B12704",
-                            color: "#fff",
-                          }}
-                        >
-                          {item?.discountPercentage}
-                          <b>%</b>
-                        </Button>
-                        <Typography>off</Typography>
-                      </Typography>
-                      <Typography component="div" sx={{ display: "flex" }}>
-                        <DoneIcon sx={{ color: "#DC9622" }} />
-                        <Typography variant="h6" sx={{ fontSize: "13px" }}>
-                          <b style={{ color: "#007185" }}>prime</b> Get it by
-                          Tomorrow, 13 March <br></br>FREE Delivery over ₹499.
-                          Fulfilled by Amazon
+                        <Typography variant="h5" sx={{ display: "flex" }}>
+                          <Rating
+                            name="simple-controlled"
+                            value={value}
+                            onChange={(event, newValue) => {
+                              setValue(newValue);
+                            }}
+                          />
+                          <Typography
+                            component="div"
+                            sx={{
+                              marginLeft: "20px",
+                              color: "#007185",
+                              fontSize: "12px",
+                            }}
+                          >
+                            {item?.rating}
+                          </Typography>
                         </Typography>
-                      </Typography>
-                    </CardActions>
-                  </Card>
+                        <Typography variant="h5" sx={{ display: "flex" }}>
+                          <Button
+                            sx={{
+                              size: "small",
 
-                  {/* <Item>xs=2</Item> */}
-                </Grid>
-              </>
-            );
-          })}
-          {/* ))} */}
-        </Grid>
-      </Box>
+                              background: "#B12704",
+                              color: "#fff",
+                            }}
+                          >
+                            {item?.discountPercentage}
+                            <b>%</b>
+                          </Button>
+                          <Typography>off</Typography>
+                        </Typography>
+                        <Typography component="div" sx={{ display: "flex" }}>
+                          <DoneIcon sx={{ color: "#DC9622" }} />
+                          <Typography variant="h6" sx={{ fontSize: "13px" }}>
+                            <b style={{ color: "#007185" }}>prime</b> Get it by
+                            Tomorrow, 13 March <br></br>FREE Delivery over ₹499.
+                            Fulfilled by Amazon
+                          </Typography>
+                        </Typography>
+                      </CardActions>
+                    </Card>
+
+                    {/* <Item>xs=2</Item> */}
+                  </Grid>
+                </>
+              );
+            })}
+            {/* ))} */}
+          </Grid>
+        </Box>
+      ) : (
+        <Box display="flex" justifyContent="center" alignItems="center">
+          <CircularProgress />
+        </Box>
+      )}
     </>
   );
 };
